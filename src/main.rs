83bf71778bbd4b43f1cfb002ebd48d88ed91e4eb
udon <myudongen@gmail.com>
2020-11-03 16:38:38 +0900

    rendering ui with prefab, impl title label to start game

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,8 @@ use amethyst::{
 use crate::bundle::GameBundle;
 use crate::state::game::Game;
 use crate::state::title::Title;
+use amethyst::ui::{RenderUi, UiBundle};
+use amethyst::input::StringBindings;
 
 mod bundle;
 mod asset;
@@ -26,13 +28,15 @@ fn main() -> amethyst::Result<()> {
     let game_data = GameDataBuilder::default()
         .with_bundle(GameBundle)?
         .with_bundle(TransformBundle::new())?
+        .with_bundle(input::bundle::create_input_bundle())?
+        .with_bundle(UiBundle::<StringBindings>::new())?
         .with_bundle(RenderingBundle::<DefaultBackend>::new()
             .with_plugin(
                 RenderToWindow::from_config_path(display_config_path)?.with_clear([0.,0.,0.,1.])
             )
+            .with_plugin(RenderUi::default())
             .with_plugin(RenderFlat2D::default())
-        )?
-        .with_bundle(input::bundle::create_input_bundle())?;
+        )?;
 
     Application::new(assets_dir, Title, game_data)?.run();
     Ok(())
--- src/state/title.rs
+++ src/state/title.rs
@@ -1,4 +1,12 @@
-use amethyst::{SimpleState, StateData, GameData, ui::{Anchor, UiLabelBuilder, UiEvent}, StateEvent, Trans, SimpleTrans};
+use amethyst::{
+    SimpleState, 
+    StateData, 
+    GameData,
+    StateEvent, 
+    Trans, 
+    SimpleTrans,
+    ui::{Anchor, UiLabelBuilder, UiEvent, UiCreator},
+};
 use crate::components::*;
 
 #[derive(Default)]
@@ -6,13 +14,17 @@ pub struct Title;
 
 impl SimpleState for Title {
     fn on_start(&mut self, data: StateData<'_, GameData<'_, '_>>) {
-        stage::initialize(data.world);
+        let StateData { mut world, .. } = data;
         
-        UiLabelBuilder::<String>::new("Game Start".to_string())
-            .with_font_size(32.0)
-            .with_position(0., 120.)
-            .with_anchor(Anchor::BottomMiddle)
-            .build_from_world(&data.world);
+        stage::initialize(world);
+        player::initialize(world);
+        block::initialize(world);
+        score::initialize(world);
+        
+        world.exec(|mut creator: UiCreator<'_>| {
+            
+            creator.create("ui/title/screen.ron", ());
+        });
     }
 
     fn handle_event(&mut self, _: StateData<'_, GameData<'_, '_>>, event: StateEvent) -> SimpleTrans {
